47d3555fde2eee15a5f64bd8ed60fb84ae39cbad
Jordan <jordanjtram@gmail.com>
2021-10-10 15:59:08 -0400

    Initial code commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Display from './components/Display';
+import Keypad from './components/Keypad';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Display />
+      <Keypad />
     </div>
   );
 }
